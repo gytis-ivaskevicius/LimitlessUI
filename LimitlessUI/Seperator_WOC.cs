d7859f025f2cc06cdcd986b60fbea2fef873cc43
WithoutCaps <withoutcapsdev@gmail.com>
2017-02-22 21:05:04 -0100

    Added animator (Desc)
    for the first time i did some sort of animation, this animation is
    mainly created for my new component that is comming up!!

--- LimitlessUI/Seperator_WOC.cs
+++ LimitlessUI/Seperator_WOC.cs
@@ -6,6 +6,45 @@ public partial class Separator_WOC : Control
 {
     private float _thikness = 1;
     private Color _lineColor = Color.DimGray;
+    private Timer _timer;
+    private bool _animationEnabled = false;
+    private int _val = 0;
+    private int _progress = 3;
+
+    public Separator_WOC()
+    {
+        DoubleBuffered = true;
+        _timer = new Timer();
+        _timer.Tick += timerTick;
+    }
+
+    public void startAnimating(int interval, int progress, int startValue)
+    {
+        _timer.Interval = interval;
+        _progress = progress;
+        if (startValue >= 0)
+            _val = startValue;
+        if (!_timer.Enabled)
+            _timer.Start();
+    }
+
+    public void stopAnimating(int value)
+    {
+        _timer.Stop();
+        _val = value;
+        Invalidate();
+    }
+
+    private void timerTick(object sender, EventArgs e)
+    {
+        _val += _progress;
+        Invalidate();
+        if (_val > Width - (Padding.Right + Padding.Left) || _val < 0)
+        {
+            _timer.Stop();
+            _val = 0;
+        }
+    }
 
     protected override void OnPaddingChanged(EventArgs e)
     {
@@ -17,6 +56,14 @@ public partial class Separator_WOC : Control
     {
         base.OnPaint(pe);
         pe.Graphics.DrawLine(new Pen(_lineColor, _thikness), Padding.Left, this.Height / 2, this.Width - Padding.Right, this.Height / 2);
+        if (_animationEnabled)
+            pe.Graphics.DrawLine(new Pen(Color.Red, _thikness), Padding.Left + (Width - _val) / 2, this.Height / 2, (Width + _val) / 2, this.Height / 2);
+    }
+
+    public bool AnimationEnabled
+    {
+        get { return _animationEnabled; }
+        set { _animationEnabled = value; }
     }
 
     public float LineThikness
