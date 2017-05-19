75fc2a798351e7381484ff09deff1defbd7a6c7f
WithoutCaps <withoutcapsdev@gmail.com>
2017-05-19 18:48:35 +0300

    Made animation bit smoother

--- LimitlessUI/Animator_WOC.cs
+++ LimitlessUI/Animator_WOC.cs
@@ -20,8 +20,8 @@ public partial class Animator_WOC : Component
     public event onAnimationTick _onAnimationEnd_del;
     public event onAnimationTick _onAnimationStart_del;
 
-    private bool _changeSize_increase_width;
-    private bool _changeSize_increase_height;
+    private bool _changeSize_increase_width = false;
+    private bool _changeSize_increase_height = false;
     private bool _changeWidth = false;
     private bool _changeHeight = false;
 
@@ -40,19 +40,15 @@ public partial class Animator_WOC : Component
 
     private void animationTimer_Tick(object sender, EventArgs e)
     {
-        _control.SuspendLayout();
-
+        if (_control == null)
+            throw new Exception("HI!!! I'm a developer of LimitlessUI and I hacked into your system!!! So, I thought that Ill help ya debug this crap - Okay, lets see... It seems that you forgot to add a control which you want to animate... Give it a shot, I'm sure that that's the issue!!  ...OH SHIT!!! Some shit went down, see ya later!");
         if (_onAnimationStart_del != null)
             _onAnimationStart_del.Invoke(_control);
+
         if (_changeWidth)
         {
             _control.Width += _changeSize_increase_width ? _changeSize_speed : -_changeSize_speed;
-            if (_changeSize_increase_width && _control.Width >= _changeSize_size.X)
-            {
-                _control.Width = _changeSize_size.X;
-                _timer.Stop();
-            }
-            else if (!_changeSize_increase_width && _control.Width <= _changeSize_size.X)
+            if ((_changeSize_increase_width && _control.Width >= _changeSize_size.X) || (!_changeSize_increase_width && _control.Width <= _changeSize_size.X + _changeSize_speed))
             {
                 _control.Width = _changeSize_size.X;
                 _timer.Stop();
@@ -63,22 +59,17 @@ public partial class Animator_WOC : Component
         if (_changeHeight)
         {
             _control.Height += _changeSize_increase_height ? _changeSize_speed : -_changeSize_speed;
-            if (_changeSize_increase_height && _control.Height > _changeSize_size.X)
+            if ((_changeSize_increase_height && _control.Height >= _changeSize_size.Y) || (!_changeSize_increase_height && _control.Height <= _changeSize_size.Y + _changeSize_speed))
             {
-                _control.Height = _changeSize_size.X;
-                _timer.Stop();
-            }
-            else if (!_changeSize_increase_height && _control.Height < _changeSize_size.X)
-            {
-                _control.Height = _changeSize_size.X;
+                _control.Height = _changeSize_size.Y;
                 _timer.Stop();
             }
             if (_onHeightChanged_del != null)
                 _onHeightChanged_del.Invoke(_control, _changeSize_increase_height ? _changeSize_speed : -_changeSize_speed, _changeSize_increase_height);
         }
+
         if (_onAnimationEnd_del != null)
             _onAnimationEnd_del.Invoke(_control);
-        _control.ResumeLayout();
     }
 
     public void startAnimation()
@@ -94,6 +85,9 @@ public partial class Animator_WOC : Component
 
     public void changeSize(int speed, Point size)
     {
+        if (_control == null)
+            return;
+
         _changeSize_speed = speed;
         _changeSize_increase_width = _control.Width < size.X;
         _changeSize_increase_height = _control.Height < size.Y;
