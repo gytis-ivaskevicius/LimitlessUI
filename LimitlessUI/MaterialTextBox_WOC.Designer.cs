d9fe4498fd48a2206a727574cf8b4b35e6cde752
WithoutCaps <withoutcapsdev@gmail.com>
2017-05-23 22:35:22 +0300

    Fixed thikness change issue
    Note: this is renamed TextBox_WOC.cs, from now on - TextBox_WOC.cs is completely different thing

---
+++ LimitlessUI/MaterialTextBox_WOC.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace LimitlessUI
+{
+    partial class MaterialTextBox_WOC
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox = new System.Windows.Forms.RichTextBox();
+            this.seperator = new Separator_WOC();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.textBox);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.panel2.Size = new System.Drawing.Size(172, 27);
+            this.panel2.TabIndex = 0;
+            // 
+            // textBox
+            // 
+            this.textBox.BackColor = System.Drawing.Color.White;
+            this.textBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox.Location = new System.Drawing.Point(4, 0);
+            this.textBox.Name = "textBox";
+            this.textBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.textBox.Size = new System.Drawing.Size(164, 27);
+            this.textBox.TabIndex = 0;
+            this.textBox.Text = "";
+            this.textBox.MouseEnter += new System.EventHandler(this.textBox_MouseEnter);
+            this.textBox.MouseLeave += new System.EventHandler(this.textBox_MouseLeave);
+            // 
+            // seperator
+            // 
+            this.seperator.AnimationEnabled = true;
+            this.seperator.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.seperator.LineColor = System.Drawing.Color.DimGray;
+            this.seperator.LineThikness = 3F;
+            this.seperator.Location = new System.Drawing.Point(0, 24);
+            this.seperator.Name = "seperator";
+            this.seperator.Size = new System.Drawing.Size(172, 3);
+            this.seperator.TabIndex = 1;
+            this.seperator.Text = "separator_WOC2";
+            // 
+            // TextBox_WOC
+            // 
+            this.Controls.Add(this.seperator);
+            this.Controls.Add(this.panel2);
+            this.Font = new System.Drawing.Font("Segoe UI", 10.5F);
+            this.Name = "TextBox_WOC";
+            this.Size = new System.Drawing.Size(172, 27);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel2;
+        private Separator_WOC seperator;
+        private System.Windows.Forms.RichTextBox textBox;
+    }
+}
---
+++ LimitlessUI/MaterialTextBox_WOC.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LimitlessUI
+{
+    public partial class MaterialTextBox_WOC : UserControl
+    {
+        private Color _underlineColor = Color.Gray;
+        private Color _animationColor = Color.Red;
+
+        private int _interval = 17;
+        private int _step = 19;
+        private bool _focused = false;
+        public MaterialTextBox_WOC()
+        {
+            InitializeComponent();
+            textBox.GotFocus += textBox_GotFocus;
+            textBox.LostFocus += textBox_LostFocus;
+            seperator.LineColor = _underlineColor;
+        }
+
+        private void textBox_LostFocus(object sender, EventArgs e)
+        {
+            _focused = false;
+            seperator.startAnimating(_interval, -_step, -1);
+        }
+
+        private void textBox_GotFocus(object sender, EventArgs e)
+        {
+            _focused = true;
+            seperator.startAnimating(_interval, _step, -1);
+        }
+
+        private void textBox_MouseEnter(object sender, EventArgs e)
+        {
+            seperator.startAnimating(_interval, _step, -1);
+            Underline.AnimationColor = _animationColor;
+        }
+
+        private void textBox_MouseLeave(object sender, EventArgs e)
+        {
+            if (!_focused)
+                seperator.startAnimating(_interval, -_step, -1);
+            if (seperator.Val > seperator.Width - (Padding.Right + Padding.Left))
+                seperator.Val = seperator.Width - (Padding.Right + Padding.Left);
+        }
+
+        public string TextBoxText
+        {
+            get { return textBox.Text; }
+            set { textBox.Text = value; }
+        }
+
+        public Color AnimationColor
+        {
+            get { return _animationColor; }
+            set { _animationColor = value; }
+        }
+
+        public RichTextBox TextBox { get { return textBox; } }
+
+        public Separator_WOC Underline { get { return seperator; } }
+    }
+}
