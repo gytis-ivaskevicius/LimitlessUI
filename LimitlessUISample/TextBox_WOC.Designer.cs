978d2e26847551d2e5b1c28f67576cdfe800919e
WithoutCaps <withoutcapsdev@gmail.com>
2017-05-23 23:56:49 +0300

    Added new TextVBox_WOC
    It is completely new textbox, has nothing to do with material one

---
+++ LimitlessUISample/TextBox_WOC.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace LimitlessUISample
+{
+    partial class TextBox_WOC
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.textBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox.Location = new System.Drawing.Point(27, 8);
+            this.textBox.Name = "textBox";
+            this.textBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.textBox.Size = new System.Drawing.Size(100, 15);
+            this.textBox.TabIndex = 0;
+            this.textBox.Text = "";
+            // 
+            // TextBox_WOC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.textBox);
+            this.Name = "TextBox_WOC";
+            this.Padding = new System.Windows.Forms.Padding(5);
+            this.Size = new System.Drawing.Size(166, 40);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox textBox;
+    }
+}
---
+++ LimitlessUISample/TextBox_WOC.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace LimitlessUISample
+{
+    public partial class TextBox_WOC : UserControl
+    {
+        private int _borderThikness = 5;
+        private bool _multiLine = true;
+        private bool _drawBorder = false;
+
+
+        public TextBox_WOC()
+        {
+            InitializeComponent();
+            textBox.ContentsResized += richTextBox_ContentsResized;
+        }
+
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+
+            textBox.Width = Width - Padding.Left - Padding.Right;
+            textBox.Left = Padding.Left;
+            textBox.Top = (this.Height - textBox.Height) / 2;
+            centerVertical(_multiLine);
+        }
+
+        private void richTextBox_ContentsResized(object sender, ContentsResizedEventArgs e)
+        {
+            var richTextBox = (RichTextBox)sender;
+            textBox.Height = e.NewRectangle.Height;
+
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            if (_drawBorder)
+            {
+                Pen pen = new Pen(Color.Black, _borderThikness);
+                pen.Alignment = PenAlignment.Inset;
+                e.Graphics.DrawRectangle(pen, ClientRectangle);
+            }
+        }
+
+        private void centerVertical(bool center)
+        {
+            if (center)
+            {
+                textBox.Dock = DockStyle.None;
+                textBox.Location = new Point(Padding.Left, (Height - textBox.Height) / 2);
+            }
+            else textBox.Dock = DockStyle.Fill;
+        }
+
+        public bool Multiline
+        {
+            get { return _multiLine; }
+            set { _multiLine = value; centerVertical(_multiLine); textBox.Multiline = !_multiLine;
+            }
+        }
+
+
+        public bool DrawBorder
+        {
+            get { return _drawBorder; }
+            set { _drawBorder = value; Invalidate(); }
+        }
+
+
+        public int BorderThikness
+        {
+            get { return _borderThikness; }
+            set
+            {
+                _borderThikness = value;
+                Invalidate();
+            }
+        }
+
+    }
+}
