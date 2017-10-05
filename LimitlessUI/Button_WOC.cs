32902ab48f331c9c3417104704e435f10efa95b0
WithoutCaps <withoutcapsdev@gmail.com>
2017-10-05 21:21:32 +0300

    Finished Button_WOC

--- LimitlessUI/Button_WOC.cs
+++ LimitlessUI/Button_WOC.cs
@@ -1,21 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Diagnostics;
 
 namespace LimitlessUI
 {
     public partial class Button_WOC : Control
     {
-        private bool _isHovering = false;
-        private int _borderThickness = 10;
-        private int _borderThicknessByTwo = 5;
         private Color _borderColor = Color.Silver;
         private Color _onHoverBorderColor = Color.Gray;
         private Color _buttonColor = Color.Red;
@@ -23,14 +13,21 @@ namespace LimitlessUI
         private Color _textColor = Color.Black;
         private Color _onHoverTextColor = Color.Gray;
 
+        private bool _isHovering;
+        private int _borderThickness = 10;
+        private int _borderThicknessByTwo = 5;
+
+
         public Button_WOC()
         {
             DoubleBuffered = true;
-            MouseEnter += (object sender, EventArgs e) => {
+            MouseEnter += (object sender, EventArgs e) =>
+            {
                 _isHovering = true;
                 Invalidate();
             };
-            MouseLeave += (object sender, EventArgs e) => {
+            MouseLeave += (object sender, EventArgs e) =>
+            {
                 _isHovering = false;
                 Invalidate();
             };
@@ -53,9 +50,12 @@ namespace LimitlessUI
             brush = new SolidBrush(_isHovering ? _onHoverButtonColor : _buttonColor);
 
             //Inner part. Button itself
-            g.FillEllipse(brush, _borderThicknessByTwo, _borderThicknessByTwo, Height - _borderThickness, Height - _borderThickness);
-            g.FillEllipse(brush, (Width - Height) + _borderThicknessByTwo, _borderThicknessByTwo, Height - _borderThickness, Height - _borderThickness);
-            g.FillRectangle(brush, Height / 2 + _borderThicknessByTwo, _borderThicknessByTwo, Width - Height - _borderThickness, Height - _borderThickness);
+            g.FillEllipse(brush, _borderThicknessByTwo, _borderThicknessByTwo, Height - _borderThickness,
+                Height - _borderThickness);
+            g.FillEllipse(brush, (Width - Height) + _borderThicknessByTwo, _borderThicknessByTwo,
+                Height - _borderThickness, Height - _borderThickness);
+            g.FillRectangle(brush, Height / 2 + _borderThicknessByTwo, _borderThicknessByTwo,
+                Width - Height - _borderThickness, Height - _borderThickness);
 
             brush.Dispose();
             brush = new SolidBrush(_isHovering ? _onHoverTextColor : _textColor);
@@ -63,7 +63,67 @@ namespace LimitlessUI
             //Button Text
             SizeF stringSize = g.MeasureString(Text, Font);
             g.DrawString(Text, Font, brush, (Width - stringSize.Width) / 2, (Height - stringSize.Height) / 2);
+        }
 
+
+        public Color BorderColor
+        {
+            get => _borderColor;
+            set
+            {
+                _borderColor = value;
+                Invalidate();
+            }
+        }
+
+        public Color OnHoverBorderColor
+        {
+            get => _onHoverBorderColor;
+            set
+            {
+                _onHoverBorderColor = value;
+                Invalidate();
+            }
+        }
+
+        public Color ButtonColor
+        {
+            get => _buttonColor;
+            set
+            {
+                _buttonColor = value;
+                Invalidate();
+            }
+        }
+
+        public Color OnHoverButtonColor
+        {
+            get => _onHoverButtonColor;
+            set
+            {
+                _onHoverButtonColor = value;
+                Invalidate();
+            }
+        }
+
+        public Color TextColor
+        {
+            get => _textColor;
+            set
+            {
+                _textColor = value;
+                Invalidate();
+            }
+        }
+
+        public Color OnHoverTextColor
+        {
+            get => _onHoverTextColor;
+            set
+            {
+                _onHoverTextColor = value;
+                Invalidate();
+            }
         }
     }
 }
